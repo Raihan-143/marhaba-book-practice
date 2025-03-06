95375b2be137c918f83da669b7fc83bfe531eacb
Md. Raihan Hasan Rana <raihan172hasan@gmail.com>
2025-03-07 02:00:21 +0600

    Add files via upload

---
+++ javascript-from-marhaba/if-else.js
@@ -0,0 +1,33 @@
+const time=6;
+if(time<6){
+    console.log('mother gives me foods');
+}else{
+    console.log('mother gives me jharu bari');
+}
+// Output: mother gives me jharu bari
+
+//Q.2
+const login=true;
+if(login){
+    console.log('welcome to the website');
+}else{
+    console.log('Get Lost to the website');
+}
+// Output: welcome to the website
+
+//Q.3
+const iRun=5;
+if(iRun>5){
+    console.log('I get chocolate');
+}else{
+    console.log('my belly will be fat');
+}
+// Output: my belly will be fat
+
+//Q.4
+const myMarks=85;
+if(myMarks>80){
+    console.log('my father gives me a bike');
+}else{
+    console.log('my father gives me a cycle');
+}
