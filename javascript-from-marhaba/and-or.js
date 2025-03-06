09e344d8b666afb3aba028b86fccfa46dcc51a18
Md. Raihan Hasan Rana <raihan172hasan@gmail.com>
2025-03-07 02:20:45 +0600

    Add files via upload

---
+++ javascript-from-marhaba/and-or.js
@@ -0,0 +1,80 @@
+const age=18;
+const heighIncm=60;
+if(age>18 && heighIncm>60){
+    console.log("he set on the bus");
+}
+else{
+    console.log("he pushed out from the bus");
+}
+// Output: he pushed out from the bus
+// In the above example, the age is less than 18 and the height is less than 60. So, the output is "he pushed out from the bus".
+
+//Q.2
+const mathMarks=80;
+const englishMarks=85;
+if(mathMarks>80 && englishMarks>85){
+    console.log("she eligible for the admission");
+}else{
+    console.log("she not eligible for the admission");
+}
+// Output: she not eligible for the admission
+// In the above example, the math marks are less than 80 and the English marks are less than 85. So, the output is "she not eligible for the admission".
+
+//Q.3
+const gpa=5;
+const familyIncome=10000;
+if(gpa>5 && familyIncome>10000){
+    console.log("he gets the scholarship");
+}
+else{
+    console.log("he does not get the scholarship");
+}
+// Output: he does not get the scholarship
+// In the above example, the GPA is less than 5 and the family income is less than 10000. So, the output is "he does not get the scholarship".
+
+//Q.4
+const ageExamenee=30;
+const experience=2;
+if(ageExamenee<30 && experience>2){
+    console.log("he gets the job");
+}else{
+    console.log("he does not get the job");
+}
+// Output: he does not get the job
+// In the above example, the age is greater than 30 and the experience is less than 2. So, the output is "he does not get the job".
+
+//Q.5
+const eggNumbers=12;
+const hens=false;
+if(eggNumbers>12 && hens==true){
+    console.log("I cook the eggs korma");
+}
+else{
+    console.log("I eat the bread and banana");
+}
+// Output: I eat the bread and banana
+// In the above example, the egg numbers are less than 12 and the hens are false. So, the output is "I eat the bread and banana".
+
+//Q.8
+const electricity=false;
+const mobileCharge=true;
+if(electricity==false && mobileCharge==false){
+    console.log("I read the book");
+}
+else{
+    console.log("I play the video game");
+}
+// Output: I play the video game
+// In the above example, the electricity is false and the mobile charge is true. So, the output is "I play the video game".
+
+//Q.9
+const shirtPrice=1000;
+const coupon=true;
+if(shirtPrice>1000 && coupon==true){
+    console.log("he get 20 percent discount");
+}
+else{
+    console.log("he did not get any discount");
+}
+// Output: he did not get any discount
+// In the above example, the shirt price is less than 1000 and the coupon is true. So, the output is "he did not get any discount".
